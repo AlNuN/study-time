74b5f392e35f84a2320f6417703ceff4879d5fd3
Matheus Alves Nunes <matheus.alnun@gmail.com>
2020-10-07 13:35:07 -0300

    Timer has cleanup in useEffect; Added slider to set initial time

--- src/components/Watch/index.js
+++ src/components/Watch/index.js
@@ -1,40 +1,62 @@
 import React, { useEffect, useState } from 'react';
 
 export default function Watch() {
-  const [time, setTime] = useState(25 * 60 * 1000);
+  const [initialTime, setInitialTime] = useState(25);
+  const [time, setTime] = useState(initialTime * 60 * 1000);
   const [timer, setTimer] = useState(false);
 
-  function startTimer() {
+  function toggleTimer() {
     if (timer === false && time > 0) {
       setTimer(true);
+    }
+    if (timer === true && time > 0) {
+      setTimer(false);
     }
   }
 
   useEffect(() => {
-    if (timer) {
-      const id = setTimeout(() => { setTime(time - 1000); }, 1000);
-      if (time <= 0) clearTimeout(id);
+    function changeTime() {
+      if (timer) setTime(time - 1000);
     }
-  });
+    const id = setTimeout(changeTime, 1000);
+    if (time === 0) setTimer(false);
+    return () => {
+      clearTimeout(id);
+    };
+  }, [timer, time]);
 
   function twoDigits(t) {
     return (`0${t}`).slice(-2);
   }
 
   function getTime(ms) {
-    const minutes = Math.floor((ms % (1000 * 60 * 60)) / (1000 * 60));
+    const minutes = Math.floor((ms % (1000 * 60 * 95)) / (1000 * 60));
     const seconds = Math.floor((ms % (1000 * 60)) / 1000);
     return `${twoDigits(minutes)}:${twoDigits(seconds)}`;
   }
 
+  function handleInitialTime(e) {
+    if (!timer) {
+      setTime(e.target.value * 60 * 1000);
+      setInitialTime(e.target.value);
+    }
+  }
+
   return (
     <>
+      <h2>Be Productive today!</h2>
+      <label>
+        Set Time
+        <input type="range" min="5" max="90" value={initialTime} step="5" onChange={handleInitialTime} />
+      </label>
       <div>{getTime(time)}</div>
       <button
         type="button"
-        onMouseUp={startTimer}
+        onMouseUp={toggleTimer}
       >
-        Start
+        {timer
+          ? 'Stop'
+          : 'Start'}
       </button>
     </>
   );
--- src/components/Watch/index.test.js
+++ src/components/Watch/index.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  fireEvent, render,
+  fireEvent, render, screen,
 } from '@testing-library/react';
 import { act } from 'react-dom/test-utils';
 import Watch from './index';
@@ -21,23 +21,66 @@ test('renders initial time', () => {
 });
 
 test('clicking start button triggers countdown', () => {
-  const { getByText } = render(<Watch />);
-  const button = getByText(/start/i);
+  render(<Watch />);
+  const button = screen.getByRole('button', { name: /start/i });
   fireEvent.mouseUp(button);
   act(() => {
     jest.advanceTimersByTime(5000);
   });
-  const result = getByText(/24:55/i);
+  const result = screen.getByText(/24:55/i);
   expect(result).toBeInTheDocument();
 });
 
 test('countdown stops at 00:00', () => {
-  const { getByText } = render(<Watch />);
-  const button = getByText(/start/i);
+  render(<Watch />);
+  const button = screen.getByRole('button', { name: /start/i });
   fireEvent.mouseUp(button);
   act(() => {
     jest.advanceTimersByTime(26 * 60 * 1000);
   });
-  const result = getByText(/00:00/i);
+  const result = screen.getByText(/00:00/i);
+  expect(result).toBeInTheDocument();
+});
+
+test('start/stop button working', async () => {
+  render(<Watch />);
+  const startButton = screen.getByRole('button', { name: /start/i });
+  fireEvent.mouseUp(startButton);
+  act(() => {
+    jest.advanceTimersByTime(1000);
+  });
+  const result = screen.getByText(/24:59/i);
+  expect(result).toBeInTheDocument();
+  const stopButton = screen.getByRole('button', { name: /stop/i });
+  fireEvent.mouseUp(stopButton);
+  await act(async () => {
+    jest.advanceTimersByTime(2000);
+  });
+  const r = screen.getByText(/24:59/i);
+  expect(r).toBeInTheDocument();
+});
+
+test('slider that sets initial time', () => {
+  render(<Watch />);
+  const slider = screen.getByRole('slider', { name: /set time/i });
+  expect(slider).toBeInTheDocument();
+
+  fireEvent.change(slider, { target: { value: '20' } });
+  const result = screen.getByText(/20:00/i);
+  expect(result).toBeInTheDocument();
+});
+
+test('slider do not change if timer is working', () => {
+  render(<Watch />);
+  const button = screen.getByRole('button', { name: /start/i });
+  fireEvent.mouseUp(button);
+  act(() => {
+    jest.advanceTimersByTime(5000);
+  });
+  const slider = screen.getByRole('slider', { name: /set time/i });
+  expect(slider).toBeInTheDocument();
+
+  fireEvent.change(slider, { target: { value: '20' } });
+  const result = screen.getByText(/24:55/i);
   expect(result).toBeInTheDocument();
 });
