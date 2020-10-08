e0038845e315a5d0fe069d78bb011701180fb794
Matheus Alves Nunes <matheus.alnun@gmail.com>
2020-10-08 09:39:51 -0300

    App varies between work and rest time, has a modal and sound alert to show that time is over.

--- src/components/Watch/index.js
+++ src/components/Watch/index.js
@@ -1,29 +1,76 @@
 import React, { useEffect, useState } from 'react';
+import Modal from 'react-modal';
+import UIfx from 'uifx';
+import alarmMp3 from '../../assets/alarm.mp3';
+
+Modal.setAppElement(document.getElementById('root'));
+
+const alarmSound = new UIfx(
+  alarmMp3,
+  {},
+);
 
 export default function Watch() {
   const [initialTime, setInitialTime] = useState(25);
   const [time, setTime] = useState(initialTime * 60 * 1000);
-  const [timer, setTimer] = useState(false);
+  const [isTimerRunning, setTimerRunning] = useState(false);
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [inRest, setInRest] = useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+    if (!inRest) {
+      setInRest(true);
+      setInitialTime(5);
+      setTime(5 * 60 * 1000);
+    } else {
+      setInRest(false);
+      setInitialTime(25);
+      setTime(25 * 60 * 1000);
+    }
+  }
+
+  function skipRest() {
+    setTimerRunning(false);
+    setInRest(false);
+    setInitialTime(25);
+    setTime(25 * 60 * 1000);
+  }
 
   function toggleTimer() {
-    if (timer === false && time > 0) {
-      setTimer(true);
+    if (time > 0) {
+      if (isTimerRunning) {
+        setTimerRunning(false);
+        setTime(initialTime * 60 * 1000);
+      } else setTimerRunning(true);
     }
-    if (timer === true && time > 0) {
-      setTimer(false);
+  }
+
+  function handleInitialTime(e) {
+    if (!isTimerRunning) {
+      setTime(e.target.value * 60 * 1000);
+      setInitialTime(e.target.value);
     }
   }
 
   useEffect(() => {
     function changeTime() {
-      if (timer) setTime(time - 1000);
+      if (isTimerRunning) setTime(time - 1000);
     }
     const id = setTimeout(changeTime, 1000);
-    if (time === 0) setTimer(false);
+    if (time <= 0) {
+      setTimerRunning(false);
+      alarmSound.play();
+      openModal();
+    }
     return () => {
       clearTimeout(id);
     };
-  }, [timer, time]);
+  }, [isTimerRunning, time]);
 
   function twoDigits(t) {
     return (`0${t}`).slice(-2);
@@ -35,29 +82,59 @@ export default function Watch() {
     return `${twoDigits(minutes)}:${twoDigits(seconds)}`;
   }
 
-  function handleInitialTime(e) {
-    if (!timer) {
-      setTime(e.target.value * 60 * 1000);
-      setInitialTime(e.target.value);
-    }
-  }
-
   return (
     <>
-      <h2>Be Productive today!</h2>
+      <h2>
+        {inRest
+          ? 'Rest Time!'
+          : 'Be Productive today!'}
+      </h2>
       <label>
         Set Time
-        <input type="range" min="5" max="90" value={initialTime} step="5" onChange={handleInitialTime} />
+        <input
+          type="range"
+          min={inRest ? '1' : '5'}
+          max={inRest ? '60' : '90'}
+          step={inRest ? '1' : '5'}
+          value={initialTime}
+          onChange={handleInitialTime}
+        />
       </label>
       <div>{getTime(time)}</div>
       <button
         type="button"
         onMouseUp={toggleTimer}
       >
-        {timer
-          ? 'Stop'
+        {isTimerRunning
+          ? 'Give Up'
           : 'Start'}
       </button>
+      {inRest
+        && (
+        <button
+          type="button"
+          onMouseUp={skipRest}
+        >
+          Skip rest
+        </button>
+        )}
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Congratulations message"
+      >
+        <h3>
+          {inRest
+            ? 'Rest Time Over!'
+            : "Congratulations, you've inRest your task!"}
+        </h3>
+        <button
+          type="button"
+          onMouseUp={closeModal}
+        >
+          Close
+        </button>
+      </Modal>
     </>
   );
 }
--- src/components/Watch/index.test.js
+++ src/components/Watch/index.test.js
@@ -42,7 +42,7 @@ test('countdown stops at 00:00', () => {
   expect(result).toBeInTheDocument();
 });
 
-test('start/stop button working', async () => {
+test('start/giveup button working', async () => {
   render(<Watch />);
   const startButton = screen.getByRole('button', { name: /start/i });
   fireEvent.mouseUp(startButton);
@@ -51,12 +51,12 @@ test('start/stop button working', async () => {
   });
   const result = screen.getByText(/24:59/i);
   expect(result).toBeInTheDocument();
-  const stopButton = screen.getByRole('button', { name: /stop/i });
+  const stopButton = screen.getByRole('button', { name: /give up/i });
   fireEvent.mouseUp(stopButton);
   await act(async () => {
     jest.advanceTimersByTime(2000);
   });
-  const r = screen.getByText(/24:59/i);
+  const r = screen.getByText(/25:00/i);
   expect(r).toBeInTheDocument();
 });
 
@@ -83,4 +83,127 @@ test('slider do not change if timer is working', () => {
   fireEvent.change(slider, { target: { value: '20' } });
   const result = screen.getByText(/24:55/i);
   expect(result).toBeInTheDocument();
+});
+
+test('open modal once time has depleted', () => {
+  render(<Watch />);
+  const slider = screen.getByRole('slider', { name: /set time/i });
+  fireEvent.change(slider, { target: { value: '5' } });
+  const button = screen.getByRole('button', { name: /start/i });
+  fireEvent.mouseUp(button);
+  act(() => {
+    jest.advanceTimersByTime(10000 * 60);
+  });
+  const finalTime = screen.getByText(/00:00/i);
+  expect(finalTime).toBeInTheDocument();
+  const congratulations = screen.getByText(/Congratulations/i);
+  expect(congratulations).toBeInTheDocument();
+});
+
+test('change to rest time UI once modal is closed', () => {
+  render(<Watch />);
+  const slider = screen.getByRole('slider', { name: /set time/i });
+  fireEvent.change(slider, { target: { value: '5' } });
+  const button = screen.getByRole('button', { name: /start/i });
+  fireEvent.mouseUp(button);
+  act(() => {
+    jest.advanceTimersByTime(10000 * 60);
+  });
+  const finalTime = screen.getByText(/00:00/i);
+  expect(finalTime).toBeInTheDocument();
+  const congratulations = screen.getByText(/Congratulations/i);
+  expect(congratulations).toBeInTheDocument();
+  const closeModalButton = screen.getByRole('button', { name: /close/i });
+  fireEvent.mouseUp(closeModalButton);
+
+  const restMessage = screen.getByText(/rest time/i);
+  expect(restMessage).toBeInTheDocument();
+  const restSlider = screen.getByRole('slider', { name: /set time/i });
+  expect(restSlider).toHaveAttribute('min', expect.stringContaining('1'));
+  expect(restSlider).toHaveAttribute('max', expect.stringContaining('60'));
+  expect(restSlider).toHaveAttribute('step', expect.stringContaining('1'));
+  const newTime = screen.getByText(/05:00/i);
+  expect(newTime).toBeInTheDocument();
+});
+
+test('rest time has button that skip rest when clicked', async () => {
+  render(<Watch />);
+  const slider = screen.getByRole('slider', { name: /set time/i });
+  fireEvent.change(slider, { target: { value: '5' } });
+  const button = screen.getByRole('button', { name: /start/i });
+  fireEvent.mouseUp(button);
+  act(() => {
+    jest.advanceTimersByTime(10000 * 60);
+  });
+  const finalTime = screen.getByText(/00:00/i);
+  expect(finalTime).toBeInTheDocument();
+  const congratulations = screen.getByText(/Congratulations/i);
+  expect(congratulations).toBeInTheDocument();
+  const closeModalButton = screen.getByRole('button', { name: /close/i });
+  fireEvent.mouseUp(closeModalButton);
+
+  const restMessage = screen.getByText(/rest time/i);
+  expect(restMessage).toBeInTheDocument();
+  const restSlider = screen.getByRole('slider', { name: /set time/i });
+  expect(restSlider).toHaveAttribute('min', expect.stringContaining('1'));
+  expect(restSlider).toHaveAttribute('max', expect.stringContaining('60'));
+  expect(restSlider).toHaveAttribute('step', expect.stringContaining('1'));
+  const newTime = screen.getByText(/05:00/i);
+  expect(newTime).toBeInTheDocument();
+
+  const restStartButton = screen.getByRole('button', { name: /start/i });
+  fireEvent.mouseUp(restStartButton);
+  await act(async () => {
+    jest.advanceTimersByTime(1000 * 60);
+  });
+
+  const skipRestButton = screen.getByRole('button', { name: /skip/i });
+  fireEvent.mouseUp(skipRestButton);
+
+  await act(async () => {
+    jest.advanceTimersByTime(1000 * 60);
+  });
+  const newOldTime = screen.getByText(/25:00/i);
+  expect(newOldTime).toBeInTheDocument();
+});
+
+test('change back to normal once rest modal is closed', async () => {
+  render(<Watch />);
+  const slider = screen.getByRole('slider', { name: /set time/i });
+  fireEvent.change(slider, { target: { value: '5' } });
+  const button = screen.getByRole('button', { name: /start/i });
+  fireEvent.mouseUp(button);
+  act(() => {
+    jest.advanceTimersByTime(10000 * 60);
+  });
+  const finalTime = screen.getByText(/00:00/i);
+  expect(finalTime).toBeInTheDocument();
+  const congratulations = screen.getByText(/Congratulations/i);
+  expect(congratulations).toBeInTheDocument();
+  const closeModalButton = screen.getByRole('button', { name: /close/i });
+  fireEvent.mouseUp(closeModalButton);
+
+  const restMessage = screen.getByText(/rest time/i);
+  expect(restMessage).toBeInTheDocument();
+  const restSlider = screen.getByRole('slider', { name: /set time/i });
+  expect(restSlider).toHaveAttribute('min', expect.stringContaining('1'));
+  expect(restSlider).toHaveAttribute('max', expect.stringContaining('60'));
+  expect(restSlider).toHaveAttribute('step', expect.stringContaining('1'));
+  const newTime = screen.getByText(/05:00/i);
+  expect(newTime).toBeInTheDocument();
+
+  const restButton = screen.getByRole('button', { name: /start/i });
+  fireEvent.mouseUp(restButton);
+  await act(async () => {
+    jest.advanceTimersByTime(6000 * 60);
+  });
+  const restTimeOver = screen.getByText(/00:00/i);
+  expect(restTimeOver).toBeInTheDocument();
+  const getBack = screen.getByText(/rest time over/i);
+  expect(getBack).toBeInTheDocument();
+  const closeRestModalButton = screen.getByRole('button', { name: /close/i });
+  fireEvent.mouseUp(closeRestModalButton);
+
+  const newOldTime = screen.getByText(/25:00/i);
+  expect(newOldTime).toBeInTheDocument();
 });
